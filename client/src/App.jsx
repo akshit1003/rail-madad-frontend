f7603695c09781d4cbc4c87dba729a03a76a0ef7
tanya bhardwaj <tanyabhardwaj59@gmail.con>
2024-09-21 15:25:05 +0530

    form code

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 // import Landingp from './pages/Landing'
 import Header from './components/Header';
+import Form from './components/Form';
 // import Services from './components/Services';
 // import Footer from './components/Footer';
 
 function App() {
   return (
     <div>
-      <Header />
+      <Form/>
     </div>
   )
 }
